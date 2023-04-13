96261626e337170f93f780b929b6920c240d1f3e
Shereen Punnassery <pshereen@gmail.com>
2023-04-13 13:41:14 -0700

    Updated Object movement based on server command.
    Updated Object movement based on server command.

--- unity/PlayerMovement.cs
+++ unity/PlayerMovement.cs
@@ -5,10 +5,25 @@ using UnityEngine.Networking;
 using System.IO;
 
 
-public class MyClass
+public class PlayerInfo
 {
     public string id;
     public string name;
+    public static PlayerInfo CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<PlayerInfo>(jsonString);
+    }
+}
+public class ServerResponse
+{
+    public string command;
+    public string msg;
+
+    public int speed;
+    public static ServerResponse CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<ServerResponse>(jsonString);
+    }
 }
 
 public class PlayerMovement : MonoBehaviour
@@ -17,6 +32,9 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float movementSpeed = 6f;
     [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] float horizontalInput = 0f;
+    [SerializeField] float verticalInput = 0f;
 
     private bool isStreaming = false;
 
@@ -27,7 +45,8 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         isStreaming = true;
         StartCoroutine(StreamObject());
-
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
 
     }
@@ -35,16 +54,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // horizontalInput = Input.GetAxis("Horizontal");
+        // verticalInput = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        // rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
-        // OnResume();
+        OnResume();
         if (Input.GetKeyDown(KeyCode.P) && isStreaming)
         {
             Debug.Log("Stream stopped");
@@ -61,19 +80,34 @@ public class PlayerMovement : MonoBehaviour
 
     IEnumerator StreamObject()
     {
-        MyClass myObject = new MyClass();
+        PlayerInfo myObject = new PlayerInfo();
         myObject.id = "1";
         myObject.name = "Cylinder";
         string jsonStringTrial = JsonUtility.ToJson(myObject);
         string url = "http://localhost:7000/objects";
-        // string bodyJsonString ='{"id":"cylinder",}';
         var request = new UnityWebRequest(url, "POST");
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonStringTrial);
         request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
         yield return request.SendWebRequest();
-        Debug.Log("Status Code: " + request.downloadHandler.text);
+        var text = request.downloadHandler.text;
+        ServerResponse responseObj = JsonUtility.FromJson<ServerResponse>(text);
+        // horizontalInput = Input.GetAxis("Horizontal");
+        // verticalInput = Input.GetAxis("Vertical");
+        Debug.Log("responseObj.command: " + responseObj.command);
+        if (responseObj.command == "move")
+        {
+            rb.velocity = new Vector3((horizontalInput + .1f) * movementSpeed, rb.velocity.y, (verticalInput) * movementSpeed);
+
+        }
+        if (responseObj.command == "set_speed")
+        {
+            var speed = (float)responseObj.speed / 1000;
+            Debug.Log("Speed: " + speed);
+            rb.velocity = new Vector3((horizontalInput + speed) * movementSpeed, rb.velocity.y, (verticalInput) * movementSpeed);
+
+        }
     }
 
     // IEnumerator StreamObject()
