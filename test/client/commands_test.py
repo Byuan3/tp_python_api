1678fea819fe6dbdd79cd6b516bc23355ab5dc2e
Byuan3 <byuan@ucsd.edu>
2023-04-13 20:49:37 -0700

    Update commands_test.py

--- test/client/commands_test.py
+++ test/client/commands_test.py
@@ -1,7 +1,7 @@
 import tp_unity_ai.tp as tp
 
 if __name__ == '__main__':
-    agent = tp.interactiveGraphics.get_agent_with_name('Ball')
+    agent = tp.interactiveGraphics.get_agent_with_name('Cylinder')
     print(agent.name)
     print(agent.id)
     agent.move()
