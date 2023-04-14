2b5ec0fdb8a37b7dff3f2fa1dc6fd37655826fd7
Byuan3 <byuan@ucsd.edu>
2023-04-13 21:51:48 -0700

    Update commands_test.py

--- test/client/commands_test.py
+++ test/client/commands_test.py
@@ -1,8 +1,17 @@
 import tp_unity_ai.tp as tp
+import time
 
 if __name__ == '__main__':
     agent = tp.interactiveGraphics.get_agent_with_name('Cylinder')
     print(agent.name)
     print(agent.id)
     agent.move()
-    agent.set_speed(100)
+    time.sleep(0.3)
+    agent.turn_left()
+    time.sleep(2.9)
+    agent.turn_right()
+    time.sleep(3.5)
+    agent.turn_right()
+    agent.set_speed(15)
+    time.sleep(2)
+    agent.stop()
