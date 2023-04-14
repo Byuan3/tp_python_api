fc545cda38b9f423a1a1e0d7fe1e77a201fce0f2
Byuan3 <byuan@ucsd.edu>
2023-04-14 15:06:06 -0700

    Update commands_test.py

--- test/client/commands_test.py
+++ test/client/commands_test.py
@@ -5,13 +5,19 @@ if __name__ == '__main__':
     agent = tp.interactiveGraphics.get_agent_with_name('Cylinder')
     print(agent.name)
     print(agent.id)
+
     agent.move()
     time.sleep(0.3)
+
     agent.turn_left()
     time.sleep(2.9)
+
     agent.turn_right()
     time.sleep(3.5)
+
     agent.turn_right()
+
     agent.set_speed(15)
     time.sleep(2)
+
     agent.stop()
